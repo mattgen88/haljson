656f9b778934178da22fabbaf1a7b077d0e84a1b
Matthew General <mgeneral@synacor.com>
2017-05-21 22:41:55 -0400

    Templated shouldn't be a reference, default to false

--- links.go
+++ links.go
@@ -14,7 +14,7 @@ type Link struct {
 	Href        string  `json:"href,omitempty"`
 	HrefLang    *string `json:"hreflang,omitempty"`
 	Profile     *string `json:"profile,omitempty"`
-	Templated   *bool   `json:"templated,omitempty"`
+	Templated   bool    `json:"templated,omitempty"`
 	Title       *string `json:"title,omitempty"`
 	Type        *string `json:"type,omitempty"`
 }
@@ -177,7 +177,7 @@ func (l *Links) UnmarshalJSON(b []byte) error {
 				case TEMPLATED:
 					var templated bool
 					templated = property.(bool)
-					link.Templated = &templated
+					link.Templated = templated
 				}
 			}
 			links = append(links, &link)
